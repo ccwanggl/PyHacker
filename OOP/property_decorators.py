3f7ff29be7d22b16a09732b959a4675f0d167e81
guoliang (WORKSTATION) <guoliang.wang@cust.edu.cn>
2025-01-06 10:16:31 +0800

    update

--- OOP/property_decorators.py
+++ OOP/property_decorators.py
@@ -28,13 +28,18 @@ class Employee:
         self.first = None
         self.last = None
 
-
 emp_1 = Employee('John', 'Smith', 50000)
 
 print(emp_1.first)
 emp_1.first = 'Jim'
+
 print(emp_1.first)
 print(emp_1.email)
+
+# @property 装饰器将方法转换为属性
 print(emp_1.fullname)
 
+emp_1.fullname = 'Guoliang WANG'
+
+isinstance(emp_1, Employee)
 del emp_1.fullname
