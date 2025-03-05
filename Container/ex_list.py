0d352e24e5b1513f131125fa0223453f1af8baf7
guoliang (WORKSTATION) <guoliang.wang@cust.edu.cn>
2025-03-05 08:34:55 +0800

    update

--- Container/ex_list.py
+++ Container/ex_list.py
@@ -1,2 +1,14 @@
+courses = ['Math', 'Physical', 'Educational']
+
+# Index start with default
+for index, s in enumerate(courses):
+    print(index, s)
+
+# index start with 'start' parameter
+for index, s in enumerate(courses, start = 10):
+    print(index, s)
+
 numbers = [1, 2, 3, 4, 5]
-print(numbers)
+results = [n * 100 for n in numbers if n % 2 == 0]
+
+print(results)
