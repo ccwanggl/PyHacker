34234e53006ad8d617c4d39fefe2f110562b7a07
guoliang (WORKSTATION) <guoliang.wang@cust.edu.cn>
2025-03-05 08:59:16 +0800

    update

--- Container/ex_list.py
+++ Container/ex_list.py
@@ -12,3 +12,14 @@ numbers = [1, 2, 3, 4, 5]
 results = [n * 100 for n in numbers if n % 2 == 0]
 
 print(results)
+
+# Add element to list
+def add_list(in_func_obj):
+    print(f'In add [before]: {in_func_obj} = "{in_func_obj}"')
+    in_func_obj += ['baz']
+    print(f'In add [after]: {in_func_obj} = "{in_func_obj}"')
+
+orig_obj = ['foo', 'bar']
+print(f'Outside [before]: orig_obj = "{orig_obj}"')
+add_list(orig_obj)
+print(f'Outside [after]: orig_obj = "{orig_obj}"')
