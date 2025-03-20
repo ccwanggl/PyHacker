4e17de9e13ef7bb2ad274609b19c4919e0fe5b9e
guoliang (WORKSTATION) <guoliang.wang@cust.edu.cn>
2025-03-20 16:20:22 +0800

    list example update

--- Container/ex_list.py
+++ Container/ex_list.py
@@ -1,6 +1,6 @@
 courses = ['Math', 'Physical', 'Educational']
 
-# Index start with default
+# Index start with default and get index of the list
 for index, s in enumerate(courses):
     print(index, s)
 
@@ -9,9 +9,19 @@ for index, s in enumerate(courses, start = 10):
     print(index, s)
 
 numbers = [1, 2, 3, 4, 5]
+print("Deal list element with list comprehension")
 results = [n * 100 for n in numbers if n % 2 == 0]
-
 print(results)
+
+def remove_odd_mul_100(numbers):
+    results = []
+    for number in numbers:
+        if number % 2 == 1:
+            continue
+        results.append(number * 100)
+    return results
+print("Deal list element with function remove_odd_mul_100()")
+print(remove_odd_mul_100(numbers))
 
 # Add element to list
 def add_list(in_func_obj):
