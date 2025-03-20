5906c62521d02471d96fafc846a9378a68cb6bf1
guoliang (WORKSTATION) <guoliang.wang@cust.edu.cn>
2025-03-20 16:59:27 +0800

    update tuple example

--- Container/ex_tuple.py
+++ Container/ex_tuple.py
@@ -1,2 +1,21 @@
 name = ('foo', 'bar', 'baz')
 print(name)
+
+def get_rectangle():
+    width = 100
+    height = 200
+    return width, height
+
+result = get_rectangle()
+print(result, type(result))
+
+results = tuple((n *100 for n in range(10) if n % 2 == 0))
+print(results)
+
+user_info = ('piglei', 'MALE', 30, True)
+print(user_info[2])
+
+from collections import namedtuple
+Rectangle = namedtuple('Rectangle', 'width height')
+rect = Rectangle(100, 100)
+print(rect.width, rect.height)
