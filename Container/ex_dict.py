bedc88b67942b7229a4d3406e2ff701dd5daa4db
guoliang (WORKSTATION) <guoliang.wang@cust.edu.cn>
2025-03-20 17:08:55 +0800

    update

--- Container/ex_dict.py
+++ Container/ex_dict.py
@@ -1,3 +1,26 @@
-nameAge = {"guoliang": 40, "mohan":8}
-print(nameAge)
-print(type(nameAge))
+movie = {'name':'Burning', 'type':'movie', 'year':2018}
+
+for key in movie:
+    print(key, movie[key])
+
+for key, value in movie.items():
+    print(key, value)
+
+try:
+    rating = movie['rating']
+except KeyError:
+    rating = 0
+print(rating)
+
+print(movie.get('rating',0))
+
+movie.setdefault("actors", []).append("bob")
+movie.setdefault('actors',[]).append("mary")
+
+for key, value in movie.items():
+    print(key, value)
+
+movie.pop('actors', None)
+
+d1 = {'foo': 3, 'bar': 4}
+var = {key: value * 10 for key, value in d1.items() if key == 'foo'}
