0ab5b2c2a2c17b00de83b95099f9005e3fafc8c8
guoliang (LABTOP) <guoliang.wang@cust.edu.cn>
2025-06-11 22:01:31 +0800

    update

---
+++ Basics/Hinting-Checking-Validation.py
@@ -0,0 +1,15 @@
+from pydantic import validate_call
+
+@validate_call
+def create_user(first_name:str, last_name:str, age:int) -> dict:
+    email = f"{first_name.lower()}_{last_name.lower()}@example.com"
+
+    return {
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email,
+        'age': age,
+    }
+
+user1 :dict = create_user("Corey", "Schafer", 38)
+print(user1)
