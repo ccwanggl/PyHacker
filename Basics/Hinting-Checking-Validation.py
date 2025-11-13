876e1d78d6045e1e42261345ecd88eab1006b309
guoliang (LAPTOP) <guoliang.wang@cust.edu.cn>
2025-11-13 11:09:20 +0800

    update

--- Basics/Hinting-Checking-Validation.py
+++ Basics/Hinting-Checking-Validation.py
@@ -1,15 +1,29 @@
 from pydantic import validate_call
+from typing import NewType
+
+RGB = NewType("RGB", tuple[int, int, int])
+HSL = NewType("HSL", tuple[int, int, int])
+
+type User = dict[str, str | int | RGB | None]
+
 
 @validate_call
-def create_user(first_name:str, last_name:str, age:int) -> dict:
+def create_user(
+    first_name: str,
+    last_name: str,
+    age: int | None = None,
+    fav_color: RGB | None = None,
+) -> dict[str, str | int | None]:
     email = f"{first_name.lower()}_{last_name.lower()}@example.com"
 
     return {
-        'first_name': first_name,
-        'last_name': last_name,
-        'email': email,
-        'age': age,
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
+        "age": age,
+        "fav_color": fav_color,
     }
 
-user1 :dict = create_user("Corey", "Schafer", "38")
+
+user1: dict = create_user("Corey", "Schafer", 38, RGB((244, 223, 11)))
 print(user1)
